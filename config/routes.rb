38b1a4207eac9016d6d9ccb82b35558dd40ca4dd
Nick Pomroy <npomroy69@hotmail.com>
2016-12-06 10:03:46 +0000

    Added control panel view and action and user authentication before action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  devise_for :users
+  get 'control-panel', to: 'users#show'
   get 'products', to: 'pages#products'
   get 'news', to: 'pages#news'
   get 'faq', to: 'pages#faq'
