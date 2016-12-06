b5003283ccf2720000aff0e50daeec47a4ce8584
Nick Pomroy <npomroy69@hotmail.com>
2016-12-06 10:20:26 +0000

    Added admin to users table, admin check before action to users controller, and bare bones control panel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_admin_user
+    
+    
    def show
        @user = User.find( current_user.id)
    end
+   
+   private
+       def only_admin_user
+           @user = current_user
+           redirect_to(root_url) unless @user.admin
+       end
 end
