b613e8fba51a2b0c47d71fd55bc9e10a80342ef0
Nick Pomroy <npomroy69@hotmail.com>
2016-12-17 15:23:45 +0000

    Added event create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
    def new
       @event = Event.new 
    end
+   
+   def create
+      @event = Event.new( event_params )
+      if @event.save
+          flash[:success] = "Event created"
+          redirect_to root_path
+      else
+          flash[:error] = "Event creation failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def event_params
+           params.require(:event).permit(:name, :startdate, :details) 
+        end
 end
