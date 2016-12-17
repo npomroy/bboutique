f468ebf700db970caf9accb280b7714994887f22
Nick Pomroy <npomroy69@hotmail.com>
2016-12-17 12:14:40 +0000

    Added pattern create action

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -2,4 +2,20 @@ class PatternsController < ApplicationController
    def new
       @pattern = Pattern.new 
    end
+   
+   def create
+      @pattern = Pattern.new( pattern_params )
+      if @pattern.save
+          flash[:success] = "Pattern Created"
+          redirect_to root_path
+      else
+          flash[:error] = 'Pattern creation failed'
+          render action: :new
+      end
+   end
+   
+   private
+        def pattern_params
+           params.require(:pattern).permit(:name) 
+        end
 end
