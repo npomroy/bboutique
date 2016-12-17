5f4c27ede41a795bebd7894e2e68059180127ed1
Nick Pomroy <npomroy69@hotmail.com>
2016-12-17 15:39:06 +0000

    Added event udpate action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,6 +18,17 @@ class EventsController < ApplicationController
         @event = Event.find(params[:id]) 
    end
    
+   def update
+        @event = Event.find(params[:id])
+        if @event.update_attributes
+            flash[:success] = "Event updated"
+            redirect_to event_path(id: params[:id])
+        else
+            flash[:error] = "Event update failed"
+            render action: :show
+        end
+   end
+   
    def destroy
         Event.find(params[:id]).destroy
         redirect_to events_path
