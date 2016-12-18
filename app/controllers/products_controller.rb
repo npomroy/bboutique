a338b6678ace435e5d6df37d13c8a56ce2552741
Nick Pomroy <npomroy69@hotmail.com>
2016-12-18 00:48:33 +0000

    Added add_to_cart button on product view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,7 @@ class ProductsController < ApplicationController
    
    def show
       @product = Product.find(params[:id])
+      @shopping_cart_item = ShoppingCartItem.new
    end
    
    def update
